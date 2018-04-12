15ad74a571d1ed3ed2c73783ae89b746cb2f65ac
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-12 12:44:20 +0200

    Clase main

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,15 @@
+
+package boletin24;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Boletin24 {
+
+    
+    public static void main(String[] args) {
+        Ventana a=new Ventana();
+    }
+
+}
